19559b2acf048b059da249335bf8b785e809f479
hiplitewhat <hiplitewhat@gmail.com>
2026-05-04 03:50:04 +0800

    ci: fix android/gradlew pathing and existence check

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,1 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; ReactDOM.createRoot(document.getElementById('root')).render(<h1>AniNotify PR Build</h1>);
+import React from 'react'; import ReactDOM from 'react-dom/client'; ReactDOM.createRoot(document.getElementById('root')).render(<h1>AniNotify Live</h1>);
